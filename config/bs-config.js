9b20e299a1e7dc8df2831b23cc783d14f9d71816
santosh <santosh_citech@live.in>
2017-11-15 15:48:58 +0530

    dasdasd

--- config/bs-config.js
+++ config/bs-config.js
@@ -1,4 +1,9 @@
+"use strict";
 module.exports = {
     "port": 8888,
-    "server": { "baseDir": "./public_development" }
+    "files" : "./public_development/**/*.{js, html, css}",
+    "server": { "baseDir": "./public_development" },
+    "open": true,
+    "https" : false,
+    "browser" : ["chrome", "firefox"]
   };
