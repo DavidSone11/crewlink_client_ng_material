0d901d533ea8247cbd5677c680ebe2bbcc69c25f
santosh <santosh_citech@live.in>
2017-11-15 16:32:06 +0530

    sadasd

--- public_development/app.config.js
+++ public_development/app.config.js
@@ -1,4 +1,5 @@
-var app = angular.module('crewlinkApp',['ngMaterial', 'md.data.table']);
+var app = angular.module('crewlinkApp', ['ngMaterial','ngMessages','oc.lazyLoad',
+'ui.router','md.data.table']);
 app.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$httpProvider',
 function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider) {
 
