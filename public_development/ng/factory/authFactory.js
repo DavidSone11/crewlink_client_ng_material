6b9d69a342460deee2b92af9e67d7f20e98e10de
santosh <santosh_citech@live.in>
2017-11-15 22:12:13 +0530

    asdasd

---
+++ public_development/ng/factory/authFactory.js
@@ -0,0 +1,22 @@
+
+var app = angular.module('EMSApp');
+app.factory("httpInterceptor", ["$q", "$window", "$log",
+    function ($q, $window, $log) {
+        return {
+            "response": function (response) {
+                var responseHeaders;
+                responseHeaders = response.headers();
+                if (responseHeaders["content-type"]
+                    .indexOf("text/html") !== -1
+                    && response.data
+                    && response.data
+                        .indexOf('<meta name="unauthorized" content="true">')
+                    !== -1) {
+                    $window.location.reload();
+                    return $q.reject(response);
+                }
+                return response;
+            }
+        };
+    }
+])
