8ccac92e198d818679340e136157b920c740ac71
santosh <santosh_citech@live.in>
2017-11-20 02:14:04 +0530

    jjjjjjjjj

--- public_development/app/custom-directives/custom-tooltip.directive.js
+++ public_development/app/custom-directives/custom-tooltip.directive.js
@@ -1,11 +1,53 @@
 var app = angular.module('emsApp');
-app.directive('custom', ['$compile', function ($compile) {
+app.directive('tooltip', ['$compile', '$sce', function ($compile, $sce) {
 
     return {
+        restrict: 'EA',
+        scope: {
+            content: '=tooltipContent'
+        },
         link: function (scope, element, attr) {
-            var ratio = +(attr.custom);
 
-            element.css('width', ratio + '%');
+            
+            scope.isShow = false;
+
+            scope.Pos = function (top, left) {
+                tooltip.css({
+                    top: top + 'px',
+                    left: left + 'px',
+                });
+            };
+
+            scope.getSafeContent = function (content) {
+                console.log(content);
+                return $sce.trustAsHtml(content);
+            };
+
+            var tooltip = angular.element(
+                '<div ng-show="isShow" class="tooltip">\
+                  <span ng-bind-html="getSafeContent(content)">dasdasda</span>\
+                  <span class="arrowspan"></span>\
+              </div>'
+            );
+            angular.element(document.querySelector('body')).append(tooltip);
+
+            element.on('mouseenter', function (event) {
+                scope.isShow = true;
+                scope.$digest();
+            });
+
+            element.on('mousemove', function (event) {
+                scope.Pos(event.clientY - 20, event.clientX + 25);
+            });
+
+            element.on('mouseleave', function () {
+                scope.isShow = false;
+                scope.$digest();
+            });
+
+            /* Compile */
+
+            $compile(tooltip)(scope);
         }
     };
 
