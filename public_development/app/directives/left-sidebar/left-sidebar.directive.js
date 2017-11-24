976dbcff9bf580c6fd6d87e4f63dbf4b57709aa5
santosh <santosh_citech@live.in>
2017-11-24 18:17:21 +0530

    dasdas

--- public_development/app/directives/left-sidebar/left-sidebar.directive.js
+++ public_development/app/directives/left-sidebar/left-sidebar.directive.js
@@ -7,8 +7,18 @@ app.directive('sidebar', ['$compile', function($compile) {
 
             console.log("blank");
 
-
+            $scope.isToggleVisible = false;
             $scope.toggleSideBar = function () {
+
+                $scope.isToggleVisible = $scope.isToggleVisible ? false : true;
+                 if( $scope.isToggleVisible){
+                    angular.element(document.querySelector('[id="page-wrapper"]')).addClass('page-wrapper-no-margin page-wrapper-no-margin-animate-hide');
+                    angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).removeClass('fa fa-bars fa-2x');
+                    angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).addClass('fa fa-times fa-2x');
+                    angular.element(document.querySelector('[id="sidebar-wrapper"]')).addClass('toggle-hide-sidebar');
+                 }else{
+                    angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).addClass('fa fa-bars fa-2x');
+                 }
 
                 console.log("DASDAS");
 
