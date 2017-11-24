762c5e884bd274f6e73101eee77a72fef8e692cd
santosh <santosh_citech@live.in>
2017-11-24 18:47:09 +0530

    dsadsa

--- public_development/app/directives/left-sidebar/left-sidebar.directive.js
+++ public_development/app/directives/left-sidebar/left-sidebar.directive.js
@@ -15,9 +15,12 @@ app.directive('sidebar', ['$compile', function($compile) {
                     angular.element(document.querySelector('[id="page-wrapper"]')).addClass('page-wrapper-no-margin page-wrapper-no-margin-animate-hide');
                     angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).removeClass('fa fa-bars fa-2x');
                     angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).addClass('fa fa-times fa-2x');
-                    angular.element(document.querySelector('[id="sidebar-wrapper"]')).addClass('toggle-hide-sidebar');
+                    //var side_hide = angular.element(document.querySelector('[id="sidebar-wrapper"]'));
+                    ///side_hide.remove();
+                    angular.element(document.querySelector('[id="sidebar-wrap"]')).addClass('toggle-hide-sidebar');
                  }else{
                     angular.element(document.querySelector("#md-toggle-button #toggle-button-font-awesome")).addClass('fa fa-bars fa-2x');
+                    angular.element(document.querySelector('[id="sidebar-wrap"]')).removeClass('toggle-hide-sidebar');
                  }
 
                 console.log("DASDAS");
