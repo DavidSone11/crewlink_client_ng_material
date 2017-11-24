addb6e5456a259155d262f699080b804971bfccb
santosh <santosh_citech@live.in>
2017-11-24 15:59:02 +0530

    sdsdasda

--- public_development/app/app.route.js
+++ public_development/app/app.route.js
@@ -1,82 +1,96 @@
 (function () {
-    var app = angular.module('emsApp', ['ngMaterial','ngCookies', 'ngMessages', 'oc.lazyLoad','ngAria','ngAnimate','ui.router', 'md.data.table','login','smart-table']);
-    app.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$httpProvider','$mdThemingProvider','$mdIconProvider',
-        function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider,$mdThemingProvider,$mdIconProvider) {
-            
+    var app = angular.module('emsApp', ['ngMaterial', 'ngCookies', 'ngMessages', 'oc.lazyLoad', 'ngAria', 'ngAnimate', 'ui.router', 'md.data.table', 'login', 'smart-table']);
+    app.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$httpProvider', '$mdThemingProvider', '$mdIconProvider',
+        function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider, $mdThemingProvider, $mdIconProvider) {
+
 
             $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('orange')
-            .backgroundPalette('grey');
-      
+                .primaryPalette('blue')
+                .accentPalette('orange')
+                .backgroundPalette('grey');
+
             $mdIconProvider.iconSet("account", '/icons/ic_account_box_white_48px.svg', 24)
-         
-            
-           // $mdThemingProvider.theme('default')
-           // .primaryPalette('green')
-           // .accentColor('pink');
+
+
+            // $mdThemingProvider.theme('default')
+            // .primaryPalette('green')
+            // .accentColor('pink');
             //$mdThemingProvider.theme('default')
             //.primaryPalette('green')
-           // .accentPalette('orange');
+            // .accentPalette('orange');
             $httpProvider.defaults.withCredentials = true;
-           ///$urlRouterProvider.otherwise('/home/dashboard');
+            ///$urlRouterProvider.otherwise('/home/dashboard');
             ///$httpProvider.interceptors.push("httpInterceptor");
 
             $stateProvider
-            .state('dashboard',
-            {
-                template: '<dashboard></dashboard>',
-                url: '/dashboard',
-                resolve: {
-                    loadMyDirectives: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load(
-                            {
+                .state('dashboard',
+                {
+                    template: '<dashboard></dashboard>',
+                    url: '/dashboard',
+                    resolve: {
+                        loadMyDirectives: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load(
+                                {
+                                    name: 'emsApp',
+                                    files: [
+                                        'app/directives/dashboard/dashboard.directive.js',
+                                        'app/directives/header/header.directive.js',
+                                        'app/directives/left-sidebar/left-sidebar.directive.js',
+                                        'app/directives/sub-header/sub-header.directive.js'
+
+                                    ]
+                                })
+                        }
+                    }
+                }).state('dashboard.home', {
+                    template: '<home></home>',
+                    url: '/home',
+                    resolve: {
+                        loadMyDirectives: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load(
+                                {
+                                    name: 'emsApp',
+                                    files: [
+                                        'app/directives/home/home.directive.js',
+                                        'app/utility/serverFetch.js',
+                                        'app/custom-directives/custom-tooltip.directive.js'
+
+                                    ]
+                                })
+                        }
+                    }
+                }).state('dashboard.blank', {
+                    template: '<blank></blank>',
+                    url: '/blank',
+                    resolve: {
+                        loadMyDirectives: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load(
+                                {
+                                    name: 'emsApp',
+                                    files: [
+                                        'app/directives/blank/blank.directive.js',
+
+
+
+                                    ]
+                                })
+                        }
+                    }
+                }).state('dashboard.table', {
+                    template: '<table></table>',
+                    url: '/table',
+                    resolve: {
+                        loadMyDirectives: function ($ocLazyLoad) {
+                            return $ocLazyLoad.load({
                                 name: 'emsApp',
                                 files: [
-                                    'app/directives/dashboard/dashboard.directive.js',
-                                    'app/directives/header/header.directive.js',
-                                    'app/directives/left-sidebar/left-sidebar.directive.js',
-                                    'app/directives/sub-header/sub-header.directive.js'
-                                  
+                                    'app/directives/table/table.directive.js',
+
                                 ]
                             })
+                        }
                     }
-                }
-            }).state('dashboard.home', {
-                template: '<home></home>',
-                url: '/home',
-                resolve: {
-                    loadMyDirectives: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load(
-                            {
-                                name: 'emsApp',
-                                files: [
-                                    'app/directives/home/home.directive.js',
-                                    'app/utility/serverFetch.js',
-                                    'app/custom-directives/custom-tooltip.directive.js'
-                                    
-                                ]
-                            })
-                    }
-                }
-            }).state('dashboard.blank', {
-                template: '<blank></blank>',
-                url: '/blank',
-                resolve: {
-                    loadMyDirectives: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load(
-                            {
-                                name: 'emsApp',
-                                files: [
-                                    'app/directives/blank/blank.directive.js',
-                                    
-                                    
-                                    
-                                ]
-                            })
-                    }
-                }
-            });
+                });
 
         }]);
 
--- public_development/app/directives/home/home.directive.js
+++ public_development/app/directives/home/home.directive.js
@@ -6,64 +6,7 @@ app.directive('home', ['$compile', function ($compile) {
         controller: function ($scope, $state, $http, $log, $q, $timeout, $window, $mdEditDialog, $location, $cookies, $cookieStore) {
 
 
-            $scope.users = [];
-            $scope.query = {
-                limit:10,
-                page:1,
-            }
-
-            $scope.getUser = function () {
-
-                $http.get("/json-data/data.json")
-                    .then(function (response) {
-                        $scope.users = response.data.results;
-                       
-                    },function(data){
-                        console.log("Error getting data from ");
-                    })
-                   
-            }
-
-            $scope.getUser();
-
-            /* var expired = new Date();
-             console.log("" + expired.getTime());
-             expired.setTime(expired.getTime() + (60 * 1));
-             $cookieStore.put('user', user.username, { expires: expired });
-             $timeout(function () {
-                 if (!$cookieStore.get('user')) {
-                     $window.location.href = '/login';
-                 }
- 
- 
-             }, 60); 
-             */
-
-            var uri = "http://localhost:8080/api/v1/user";
-            var servFetch = new serverFetch(uri, [], $http,
-                function errorCallBackBefore(response) {
-                    console.log("dsaad" + response);
-                },
-                function successCallBackAfter(response) {
-                    console.log("eee" + response);
-                },
-                function callBackAfterError(response) {
-                    console.log("eee" + response);
-                },
-                true
-
-            )
-
-            //servFetch.add();
-
-            $timeout(function () {
-                if (!$cookieStore.get('user')) {
-                    $window.location.href = '/login';
-                }
-
-
-            }, 60);
-
+           
 
 
         }
