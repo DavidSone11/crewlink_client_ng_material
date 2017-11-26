7de734c12207a0d011b3ad9bff41e99a98184b78
santosh <santosh_citech@live.in>
2017-11-26 17:14:55 +0530

    sssss

--- GruntFile.js
+++ GruntFile.js
@@ -1,34 +1,54 @@
 module.exports = function (grunt) {
+    var input = "public_dev/";
+    var output = "public/";
+    grunt.initConfig({
+        concat: {
+            javascripts: {
+                src: 'public_dev/js/**/*.js',
+                dest: 'public/javascripts/build.js'
+            },
+            styles: {
+                src: 'public_dev/stylesheets/**/*.css',
+                dest: 'public/stylesheets/build.css'
+            }
+        }       
+    });
+
+    grunt.registerTask("speaking", function () {
+        uglify: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: 'public_dev/js/**/*.js',
+                    dest: 'public/javascripts/build1.js',
+                    cwd: 'public_dev/app'
+                }]
+            }
+        }
+
+    });
+
+    grunt.registerTask("yelling", function () {
+
+
+
+    });
+
+    grunt.registerTask("clean:output",function(){
+        clean: {
+            build: {
+                src: [output+'javascripts/**/*.js', output+'stylesheets/**/*.css', output+'images/**/*.+(png|jpg|jpeg|gif)']
+            }
+        }
+
+        
+    });
+
+    grunt.registerTask("default", ['speaking', 'yelling', 'concat']);
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-
-    var inPut_folder = "public_dev";
-    var outPut_folder = "public";
-
-    grunt.initConfig({
-        concat: {
-            dist: {
-                src: ['/public_dev/js/**/*.js'],
-                dest: ['/public/javascripts/build.js']
-            }
-        }
-    });
-
-    grunt.registerTask("speak", function () {
-
-
-
-    });
-
-    grunt.registerTask("yell", function () {
-
-
-
-    });
-
-    grunt.registerTask("default", ['speak', 'yell']);
-
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
 }
