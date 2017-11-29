e62bf1c71b0f632f18ef604c23b937184bee13f5
santosh <santosh_citech@live.in>
2017-11-29 20:01:52 +0530

    dasdasd

--- public_dev/app/directives/home/home.directive.js
+++ public_dev/app/directives/home/home.directive.js
@@ -6,7 +6,25 @@ app.directive('home', ['$compile', function ($compile) {
         controller: function ($scope, $state, $http, $log, $q, $timeout, $window, $mdEditDialog, $location, $cookies, $cookieStore) {
 
 
-           
+             $scope.generateUUID = function()
+            {
+                var d = new Date().getTime();
+                
+                if( window.performance && typeof window.performance.now === "function" )
+                {
+                    d += performance.now();
+                }
+                
+                var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+                {
+                    var r = (d + Math.random()*16)%16 | 0;
+                    d = Math.floor(d/16);
+                    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+                });
+            
+               $scope.uuidkey =  uuid;
+            }
+            
 
 
         }
